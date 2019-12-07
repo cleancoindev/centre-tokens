4cd0cffbb7ee0b627901310a174c79e2700db139
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2019-12-07 10:32:24 -1000

    Update and rename 2_deploy_usdc.js to 2_deploy_fco2.js
    Needs to be updated before creation of token to change admin addy, minting addy, etc.

--- migrations/2_deploy_fco2.js
+++ migrations/2_deploy_fco2.js
@@ -2,7 +2,7 @@ var FiatTokenV1 = artifacts.require("./FiatTokenV1.sol");
 var FiatTokenProxy = artifacts.require("./FiatTokenProxy.sol");
 
 // Any address will do, preferably one we generated
-var throwawayAddress = "0x64e078a8aa15a41b85890265648e965de686bae6";
+var throwawayAddress = "0x64e078a8aa15a41b85890265648e965de686bae6";  //TODO:  Change me!!!
 
 module.exports = function(deployer, network, accounts) {
 
@@ -10,11 +10,11 @@ module.exports = function(deployer, network, accounts) {
         // Change these to the cold storage addresses provided by ops
         // these are the deterministic addresses from ganache, so the private keys are well known
         // and match the values we use in the tests
-        var admin = "0x2f560290fef1b3ada194b6aa9c40aa71f8e95598";
-        var masterMinter = "0x3e5e9111ae8eb78fe1cc3bb8915d5d461f3ef9a9";
-        var pauser = "0xaca94ef8bd5ffee41947b4585a84bda5a3d3da6e";
-        var blacklister = "0xd03ea8624c8c5987235048901fb614fdca89b117";
-        var owner = "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";
+        var admin = "0x2f560290fef1b3ada194b6aa9c40aa71f8e95598";  //TODO:  Change me!!!
+        var masterMinter = "0x3e5e9111ae8eb78fe1cc3bb8915d5d461f3ef9a9";  //TODO:  Change me!!!
+        var pauser = "0xaca94ef8bd5ffee41947b4585a84bda5a3d3da6e";  //TODO:  Change me!!!
+        var blacklister = "0xd03ea8624c8c5987235048901fb614fdca89b117";  //TODO:  Change me!!!
+        var owner = "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";  //TODO:  Change me!!!
     }
 
     console.log("deploying impl")
@@ -54,9 +54,9 @@ module.exports = function(deployer, network, accounts) {
             // this is fine because the proxy will forward all the calls to the FiatTokenV1 impl
             tokenProxy = FiatTokenV1.at(tokenProxy.address);
             return tokenProxy.initialize(
-                "USD//C",
-                "USDC",
-                "USD",
+                "USD//C",  //TODO:  Change me!!!  change to F//Co2 ?
+                "USDC",  //TODO:  Change me!!!  change to FCO2
+                "USD",  // TODO:  do NOT change me.  i am the pegged currency for the fiat token.  we are pegging to USD i believe?
                 6,
                 masterMinter,
                 pauser,
